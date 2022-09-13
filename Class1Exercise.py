cd9aa3612499c9c0d0c6cd1216d089bf3723513a
JIEKUN LIU <61619839+jkL970910@users.noreply.github.com>
2022-09-13 19:55:46 -0400

    Update Class1Exercise.py
    add a new variable 3 in haoming's file by Jiekun Liu

--- Class1Exercise.py
+++ Class1Exercise.py
@@ -1,5 +1,6 @@
-user_input_1 = input("please enter your first value: ")
-user_input_2 = input("please enter your second value: ")
+user_input_1 = float(input("please enter your first value: "))
+user_input_2 = float(input("please enter your second value: "))
+user_input_3 = float(input("please enter your second value: "))
 
 output_operation_1 = user_input_1 + user_input_2
 output_operation_2 = user_input_1 - user_input_2
@@ -8,3 +9,10 @@ output_operation_4 = user_input_1 ** user_input_2
 output_operation_5 = user_input_1 / user_input_2
 
 print(output_operation_1, output_operation_2, output_operation_2, output_operation_2, output_operation_2)
+
+new_plus_output = plus_output + c
+new_minus_output = minus_output - c
+new_divide_output = divide_output / c
+new_surplus_output = surplus_output * c
+new_cube_output = cube_output ** c
+print(new_plus_output, new_minus_output, new_divide_output, new_surplus_output, new_cube_output)
